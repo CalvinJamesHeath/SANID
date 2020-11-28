e36ba9195a30ebc3f9165d7c8eddcab433a0d97c
CalvinJamesHeath <CalvinJamesHeath14@hotmail.com>
2020-11-27 21:50:48 -0300

    first commit

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+  {
+    title: "Inicio",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Servicios",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Productos",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Contáctanos",
+    url: "#",
+    cName: "nav-links-mobile",
+  },
+];
