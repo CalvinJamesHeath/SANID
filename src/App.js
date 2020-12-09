7c4e7ceaf69afb036d99c5d3dc99ed103ffde0f8
CalvinJamesHeath <CalvinJamesHeath14@hotmail.com>
2020-12-09 20:31:28 -0300

    wp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Desinfecciones from "./components/Pages/Desinfecciones";
@@ -11,6 +16,7 @@ import OtrosServicios from "./components/Navbar/OtrosServicios";
 function App() {
   return (
     <Router>
+      <Redirect to="/" component={Inicio} />
       <div className="App">
         <Navbar />
         <Switch>
@@ -19,6 +25,16 @@ function App() {
           <Route path="/Matafuegos" component={Productos} />
           <Route path="/OtrosServicios" component={OtrosServicios} />
         </Switch>
+        <div className="whatsapp">
+          <a
+            href="https://wa.me/+5493541533876"
+            class="whatsapp_float"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i class="fa fa-whatsapp whatsapp-icon"></i>
+          </a>
+        </div>
         <Footer />
       </div>
     </Router>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,76 +1,128 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Footer.css";
 import Grid from "@material-ui/core/Grid";
+import axios from "axios";
 
-function Footer() {
-  return (
-    <React.Fragment>
-      <div className="container">
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={12} md={8} lg={8}>
-            <h3>
-              <strong>Nuestra Ubicación</strong>
-            </h3>
+const API_PATH =
+  "http://localhost:1992/react-contact-form/api/contact/index.php";
 
-            <div id="wrapper">
-              <iframe
-                title="map"
-                width="100%"
-                height="468"
-                frameBorder="0"
-                scrolling="no"
-                marginHeight="0"
-                marginWidth="0"
-                // zIndex="0"
-                src="https://maps.google.com/maps?width=100%25&amp;height=300&amp;hl=en&amp;q=Sanid%20SA+(Sanid)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-              ></iframe>
-            </div>
+class Footer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fname: "",
+      lname: "",
+      email: "",
+      message: "",
+      mailSent: false,
+      error: null,
+    };
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    axios({
+      method: "post",
+      url: `${API_PATH}`,
+      headers: { "content-type": "application/json" },
+      data: this.state,
+    })
+      .then((result) => {
+        this.setState({
+          mailSent: result.data.sent,
+        });
+      })
+      .catch((error) => this.setState({ error: error.message }));
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="container">
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={12} md={8} lg={8}>
+              <h3>
+                <strong>Nuestra Ubicación</strong>
+              </h3>
+
+              <div id="wrapper">
+                <iframe
+                  title="map"
+                  width="100%"
+                  height="468"
+                  frameBorder="0"
+                  scrolling="no"
+                  marginHeight="0"
+                  marginWidth="0"
+                  // zIndex="0"
+                  src="https://maps.google.com/maps?width=100%25&amp;height=300&amp;hl=en&amp;q=Sanid%20SA+(Sanid)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+                ></iframe>
+              </div>
+            </Grid>
+            <Grid item xs={12} sm={12} md={4} lg={4}>
+              <h3>
+                <strong>Contáctanos!</strong>
+              </h3>
+              <div>
+                <form action="/action_page.php">
+                  <label>Nombre</label>
+                  <input
+                    type="text"
+                    id="fname"
+                    name="firstname"
+                    placeholder="Tu nombre..."
+                    value={this.state.fname}
+                    onChange={(e) => this.setState({ fname: e.target.value })}
+                  />
+                  <label>Apellido</label>
+                  <input
+                    type="text"
+                    id="lname"
+                    name="lastname"
+                    placeholder="Tu apellido..."
+                    value={this.state.lname}
+                    onChange={(e) => this.setState({ lname: e.target.value })}
+                  />
+
+                  <label>Email</label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="Tu email..."
+                    value={this.state.email}
+                    onChange={(e) => this.setState({ email: e.target.value })}
+                  />
+
+                  <label>Mensaje</label>
+                  <textarea
+                    id="subject"
+                    name="subject"
+                    placeholder="Escribe algo..."
+                    onChange={(e) => this.setState({ message: e.target.value })}
+                    value={this.state.message}
+                  ></textarea>
+                  <div id="center">
+                    <input
+                      type="submit"
+                      value="Enviar"
+                      onClick={(e) => this.handleFormSubmit(e)}
+                    />
+                  </div>
+                  <div>
+                    {this.state.mailSent && (
+                      <div>Gracias por Contactarte con Nosotros</div>
+                    )}
+                  </div>
+                </form>
+              </div>
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={12} md={4} lg={4}>
-            <h3>
-              <strong>Contáctanos!</strong>
-            </h3>
-            <div>
-              <form action="/action_page.php">
-                <label>Nombre</label>
-                <input
-                  type="text"
-                  id="fname"
-                  name="firstname"
-                  placeholder="Tu nombre..."
-                />
-                <label>Apellido</label>
-                <input
-                  type="text"
-                  id="lname"
-                  name="lastname"
-                  placeholder="Tu apellido..."
-                />
-
-                <label>Email</label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="Tu email..."
-                />
-
-                <label>Mensaje</label>
-                <textarea
-                  id="subject"
-                  name="subject"
-                  placeholder="Escribe algo..."
-                ></textarea>
-                <div id="center">
-                  <input type="submit" value="Enviar" />
-                </div>
-              </form>
-            </div>
-          </Grid>
-        </Grid>
-      </div>
-    </React.Fragment>
-  );
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Footer;
