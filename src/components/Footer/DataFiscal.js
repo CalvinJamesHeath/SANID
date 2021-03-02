10fb0aa195eb8e6cd8802f7ca3937062e8f9dee5
CalvinJamesHeath <CalvinJamesHeath14@hotmail.com>
2021-03-02 17:40:05 -0300

    mobile fixes

--- src/components/Footer/DataFiscal.js
+++ src/components/Footer/DataFiscal.js
@@ -1,27 +1,30 @@
 import React from "react";
 import "./DataFiscal.css";
 import Fiscal from "./dataFiscal.jpg";
+import Grid from "@material-ui/core/Grid";
 
 export default function DataFiscal() {
   return (
-    <>
-      <div className="card" style={{ textAlign: "center" }}>
-        <img src={Fiscal} alt="Fiscal" />
-        <div className="card-body">
-          <h5 className="card-title titulo-fiscal">
-            Administración Federal de Ingresos Públicos
-          </h5>
-          <p className="card-text">VENTA DE INSUMOS PARA SEGURIDAD E HIGIENE</p>
-          <a
-            href="https://www.afip.gob.ar/sitio/externos/default.asp"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="btn btn-primary"
-          >
-            AFIP
-          </a>
+    <Grid className="dtf" container spacing={0}>
+      <Grid item md={12}>
+        <div className="card__imgc">
+          <img src={Fiscal} alt="Fiscal" className="fiscal" />
         </div>
+      </Grid>
+      <div className="card-body">
+        <h5 className="card-title titulo-fiscal">
+          Administración Federal de Ingresos Públicos
+        </h5>
+        <p className="card-text">VENTA DE INSUMOS PARA SEGURIDAD E HIGIENE</p>
+        <a
+          href="https://www.afip.gob.ar/sitio/externos/default.asp"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn btn-primary"
+        >
+          AFIP
+        </a>
       </div>
-    </>
+    </Grid>
   );
 }
